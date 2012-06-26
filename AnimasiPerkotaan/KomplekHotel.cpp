fd8a1836133b88cd87e8fa37e44deae9797df0fc
kaharisman <kaharisman@rocketmail.com>
2012-06-26 10:06:59 +0700

    Second Commit

--- AnimasiPerkotaan/KomplekHotel.cpp
+++ AnimasiPerkotaan/KomplekHotel.cpp
@@ -23,10 +23,11 @@
 float angle = 0;
 using namespace std;
 
+GLuint texture[2];
 float lastx, lasty;
 GLint stencilBits;
 
-static int camera_z= 150;
+static int camera_z = 150;
 static int camera_x = 0;
 static int camera_y = 50;
 static int posisi_z = 0;
@@ -35,7 +36,118 @@ static int posisi_y = 50;
 float rot = 0;
 
 
+struct Gambar {
+	unsigned long sizeX;
+	unsigned long sizeY;
+	char *data;
+};
+typedef struct Gambar Gambar; //struktur data untuk
 
+
+//ukuran gambar #bisa di set sesuai kebutuhan
+
+
+//mengambil gambar BMP
+int GambarLoad(char *filename, Gambar *gambar) {
+	FILE *file;
+	unsigned long size; // ukuran gambar dalam bytes
+	unsigned long i; // standard counter.
+	unsigned short int plane; // number of planes in gambar
+
+	unsigned short int bpp; // jumlah bits per pixel
+	char temp; // temporary color storage for var warna sementara untuk memastikan filenya ada
+
+
+	if ((file = fopen(filename, "rb")) == NULL) {
+		printf("File Not Found : %s\n", filename);
+		return 0;
+	}
+	// mencari file header bmp
+	fseek(file, 18, SEEK_CUR);
+	// read the width
+	if ((i = fread(&gambar->sizeX, 4, 1, file)) != 1) {
+		printf("Error reading width from %s.\n", filename);
+		return 0;
+	}
+	//printf("Width of %s: %lu\n", filename, gambar->sizeX);
+	// membaca nilai height
+	if ((i = fread(&gambar->sizeY, 4, 1, file)) != 1) {
+		printf("Error reading height from %s.\n", filename);
+		return 0;
+	}
+	//printf("Height of %s: %lu\n", filename, gambar->sizeY);
+	//menghitung ukuran gambar(asumsi 24 bits or 3 bytes per pixel).
+
+	size = gambar->sizeX * gambar->sizeY * 3;
+	// read the planes
+	if ((fread(&plane, 2, 1, file)) != 1) {
+		printf("Error reading planes from %s.\n", filename);
+		return 0;
+	}
+	if (plane != 1) {
+		printf("Planes from %s is not 1: %u\n", filename, plane);
+		return 0;
+	}
+	// read the bitsperpixel
+	if ((i = fread(&bpp, 2, 1, file)) != 1) {
+		printf("Error reading bpp from %s.\n", filename);
+
+		return 0;
+	}
+	if (bpp != 24) {
+		printf("Bpp from %s is not 24: %u\n", filename, bpp);
+		return 0;
+	}
+	// seek past the rest of the bitmap header.
+	fseek(file, 24, SEEK_CUR);
+	// read the data.
+	gambar->data = (char *) malloc(size);
+	if (gambar->data == NULL) {
+		printf("Error allocating memory for color-corrected gambar data");
+		return 0;
+	}
+	if ((i = fread(gambar->data, size, 1, file)) != 1) {
+		printf("Error reading gambar data from %s.\n", filename);
+		return 0;
+	}
+	for (i = 0; i < size; i += 3) { // membalikan semuan nilai warna (gbr - > rgb)
+		temp = gambar->data[i];
+		gambar->data[i] = gambar->data[i + 2];
+		gambar->data[i + 2] = temp;
+	}
+	// we're done.
+	return 1;
+}
+
+//mengambil tekstur
+Gambar * loadTexture() {
+	Gambar *gambar1;
+	// alokasi memmory untuk tekstur
+	gambar1 = (Gambar *) malloc(sizeof(Gambar));
+	if (gambar1 == NULL) {
+		printf("Error allocating space for gambar");
+		exit(0);
+	}
+	//pic.bmp is a 64x64 picture
+	if (!GambarLoad("beton.bmp", gambar1)) {
+		exit(1);
+	}
+	return gambar1;
+}
+Gambar * loadTextureDua() {
+	Gambar *gambar1;
+	// alokasi memmory untuk tekstur
+	gambar1 = (Gambar *) malloc(sizeof(Gambar));
+	if (gambar1 == NULL) {
+		printf("Error allocating space for gambar");
+		exit(0);
+	}
+	//pic.bmp is a 64x64 picture
+	if (!GambarLoad("water.bmp", gambar1)) {
+		exit(1);
+	}
+	return gambar1;
+}
 //train 2D
 //class untuk terain 2D
 class Terrain {
@@ -284,7 +396,58 @@ void drawSceneTanah(Terrain *terrain, GLfloat r, GLfloat g, GLfloat b) {
 
 unsigned int LoadTextureFromBmpFile(char *filename);
 
+void dasar()
+{
+glPushMatrix();
+glBegin(GL_POLYGON);
+glTexCoord2f(0.0, 0.0);
+glVertex3f(1, 10, 0.0);
+glTexCoord2f(1.0, 0.0);
+glVertex3f(10, 10, 0.0);
+glTexCoord2f(1.0, 1.0);
+glVertex3f(10, 20, 0.0);
+glTexCoord2f(0.0, 1.0);
+glVertex3f(1, 20, 0.0);
+glEnd();
 
+glPopMatrix();
+}
+
+void pohon(){
+	glColor3f(0.8, 0.5, 0.2);
+	//<<<<<<<<<<<<<<<<<<<< Batang >>>>>>>>>>>>>>>>>>>>>>>
+	glPushMatrix();
+	glScalef(0.2, 4, 0.2);
+	glutSolidSphere(1.0, 20, 16);
+	glPopMatrix();
+	//<<<<<<<<<<<<<<<<<<<< end Batang >>>>>>>>>>>>>>>>>>>>>>>
+
+	glColor3f(0.0, 1.0, 0.0);
+	//<<<<<<<<<<<<<<<<<<<< Daun >>>>>>>>>>>>>>>>>>>>>>>
+	glPushMatrix();
+	glScalef(1, 1, 1.0);
+	glTranslatef(0, 1, 0);
+	glRotatef(270, 1, 0, 0);
+	glutSolidCone(1,3,10,1);	
+	glPopMatrix();
+
+	glPushMatrix();
+	glScalef(1, 1, 1.0);
+	glTranslatef(0, 2, 0);
+	glRotatef(270, 1, 0, 0);
+	glutSolidCone(1,3,10,1);	
+	glPopMatrix();
+
+	glPushMatrix();
+	glScalef(1, 1, 1.0);
+	glTranslatef(0, 3, 0);
+	glRotatef(270, 1, 0, 0);
+	glutSolidCone(1,3,10,1);	
+	glPopMatrix();
+	//<<<<<<<<<<<<<<<<<<<< end Daun >>>>>>>>>>>>>>>>>>>>>>>
+
+
+}
 
 
 
@@ -300,6 +463,65 @@ void display(void) {
 	glLoadIdentity();
 	gluLookAt(camera_x, camera_y, camera_z, posisi_x, posisi_y, posisi_z, 0.0, 1.0, 0);
     
+	//jalan
+	glColor3f(0.0f, 0.0f, 0.0f);
+	glPushMatrix();
+	glTranslatef(150, -9, 800);	
+	glRotatef(270, 1.0, 0.0, 0.0);
+	glScalef(10, 45, 0);
+	dasar();
+	glPopMatrix();
+
+	//jalan
+	glColor3f(0.0f, 0.0f, 0.0f);
+	glPushMatrix();
+	glTranslatef(-220, -9, 330);	
+	glRotatef(270, 1.0, 0.0, 0.0);
+	glScalef(70, 10, 0);
+	dasar();
+	glPopMatrix();
+
+	//dasar
+	glColor3f(1.0f, 1.0f, 1.0f);
+	glPushMatrix();
+	glEnable(GL_TEXTURE_2D);
+    glBindTexture(GL_TEXTURE_2D,texture[0]);	
+	glTranslatef(-220, -10, 800);	
+	glRotatef(270, 1.0, 0.0, 0.0);
+	glScalef(70, 45, 0);
+	dasar();
+	glDisable(GL_TEXTURE_2D);
+	glPopMatrix();
+
+
+	//<<<<<<<<<<<<<<<<<<<< pohon >>>>>>>>>>>>>>>>>>>>>>>
+	glPushMatrix();
+	glColor3f(1, 0, 0);
+	glTranslatef(-110, 0, 85);
+	glScalef(10, 10, 10);	
+	pohon();
+	glPopMatrix();
+
+	glPushMatrix();
+	glColor3f(1, 0, 0);
+	glTranslatef(-110, 0, -85);
+	glScalef(10, 10, 10);	
+	pohon();
+	glPopMatrix();
+
+	glPushMatrix();
+	glColor3f(1, 0, 0);
+	glTranslatef(-110, 0, 0);
+	glScalef(10, 10, 10);	
+	pohon();
+	glPopMatrix();
+
+	
+
+	//<<<<<<<<<<<<<<<<<<<< end pohon >>>>>>>>>>>>>>>>>>>>>>>
+	
+
+
 	
 	glPushMatrix();	
 	//glBindTexture(GL_TEXTURE_3D, texture[0]);
@@ -342,6 +564,46 @@ void init(void) {
 	_terrain = loadTerrain("heightmap.bmp", 20);
 	_terrainTanah = loadTerrain("heightmapTanah.bmp", 20);
 	_terrainAir = loadTerrain("heightmapAir.bmp", 20);
+
+	//binding texture
+    //glClearColor(0.5, 0.5, 0.5, 0.0);
+	Gambar *gambar1 = loadTexture();
+	Gambar *gambar2 = loadTextureDua();
+
+	if (gambar1 == NULL) {
+		printf("Gambar was not returned from loadTexture\n");
+		exit(0);
+	}
+
+	glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+
+	// Generate texture/ membuat texture
+	glGenTextures(2, texture);
+
+	//binding texture untuk membuat texture 2D
+	glBindTexture(GL_TEXTURE_2D, texture[0]);
+
+	//menyesuaikan ukuran textur ketika gambar lebih besar dari texture
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST); //
+	//menyesuaikan ukuran textur ketika gambar lebih kecil dari texture
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST); //
+
+	glTexImage2D(GL_TEXTURE_2D, 0, 3, gambar1->sizeX, gambar1->sizeY, 0, GL_RGB,
+			GL_UNSIGNED_BYTE, gambar1->data);
+
+	//tekstur air
+
+	//binding texture untuk membuat texture 2D
+	glBindTexture(GL_TEXTURE_2D, texture[2]);
+
+	//menyesuaikan ukuran textur ketika gambar lebih besar dari texture
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR); //
+	//menyesuaikan ukuran textur ketika gambar lebih kecil dari texture
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR); //
+
+	glTexImage2D(GL_TEXTURE_2D, 0, 3, gambar2->sizeX, gambar2->sizeY, 0, GL_RGB,
+			GL_UNSIGNED_BYTE, gambar2->data);	
+
 }
 
 static void kibor(int key, int x, int y) {
@@ -365,11 +627,11 @@ static void kibor(int key, int x, int y) {
 		break;
 
 	case GLUT_KEY_UP:
-		camera_z= camera_z-15;	
+		camera_z = camera_z -15;	
 		posisi_z = posisi_z - 15;
 		break;
 	case GLUT_KEY_DOWN:
-		camera_z= camera_z+15;
+		camera_z = camera_z+15;
 		posisi_z = posisi_z + 15;
 		break;	
 	}
@@ -392,7 +654,7 @@ int main(int argc, char **argv) {
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_STENCIL | GLUT_DEPTH); //add a stencil buffer to the window
 	glutInitWindowSize(800, 600);
 	glutInitWindowPosition(100, 100);
-	glutCreateWindow("Sample Terain");
+	glutCreateWindow("Animasi Perkotaan");
 	init();
 
 	glutDisplayFunc(display);
@@ -412,3 +674,7 @@ int main(int argc, char **argv) {
 	glutMainLoop();
 	return 0;
 }
+
+
+
+
